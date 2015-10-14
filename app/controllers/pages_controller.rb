074a3b3ef4047bfb7b1b8796a55cbf63f89890f3
CaioEPS <caioergos@gmail.com>
2015-10-14 10:22:42 -0300

    title added to projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
   def home
     redirect_to projects_path if current_user.present?
   end
+
+  def dashboard
+    @customers = User.all
+    @projects = Projects.all.limit
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014120040) do
+ActiveRecord::Schema.define(version: 20151014125414) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "url"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20151014120040) do
     t.integer  "user_id"
     t.string   "name"
     t.boolean  "completed",  default: false
+    t.string   "title"
+    t.boolean  "done"
   end
 
   add_index "projects", ["user_id"], name: "index_projects_on_user_id"
