ef3ede1f69d716019cfcb496585db49553291c13
CaioEPS <caioergos@gmail.com>
2015-10-27 15:03:27 -0300

    Timezone de brasilia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  protected
 
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :name,
                                                             :contact_name, :phone,
